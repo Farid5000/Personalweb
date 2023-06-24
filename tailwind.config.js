9713dc3819e75163c2150b87c09fdd81b6d60218
Farid5000 <137290751+Farid5000@users.noreply.github.com>
2023-06-24 19:19:46 +0700

    Hero fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#9333ea",
         dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
